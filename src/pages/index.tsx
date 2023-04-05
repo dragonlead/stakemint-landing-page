d9c2f67c1d3f13e5ba739f44b30a8cedd0e504e7
Paulo Buenavente <46041015+e-t-n-e-v@users.noreply.github.com>
2023-04-05 15:44:21 +0800

    yt div fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -286,15 +286,16 @@ export default function Home() {
               The CryptoDawg app makes it easy for you to earn a yield every
               day.
             </motion.p>
-            <motion.div
-              variants={fadeInToUp}
-              initial="hidden"
-              whileInView="visible"
-            >
-              <div className="rounded-xl overflow-clip -mt-[5rem] -mb-[5rem] lg:mb-0 lg:mt-[44px] scale-[.41] md:scale-[.6] lg:scale-100">
+
+            <div className="rounded-xl overflow-clip -mt-[5rem] -mb-[5rem] lg:mb-0 lg:mt-[44px] scale-[.41] md:scale-[.6] lg:scale-100">
+              <motion.div
+                variants={fadeInToUp}
+                initial="hidden"
+                whileInView="visible"
+              >
                 <YouTube videoId="AJZN2kb1aV4" opts={opts} onReady={_onReady} />
-              </div>
-            </motion.div>
+              </motion.div>
+            </div>
           </div>
         </div>
 
